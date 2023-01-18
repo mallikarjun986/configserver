8b527c9a17a22b6ac2bc3812010ccbfeca7e1177
Mallikarjun Shurpali <mallikarjun.shurpali@gmail.com>
2023-01-18 15:51:13 +0530

    Commit config  Server Details.
    first commit of config server project.

---
+++ config-server/src/main/java/com/spring/microservices/config/server/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.spring.microservices.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
